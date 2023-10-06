7886814328c528d9e29be4c18f7666344aaaed9b
TomBim <washingtonbim@hotmail.com>
2023-10-06 19:37:27 -0300

    Added a 'main.h'

--- main.cpp
+++ main.cpp
@@ -1,24 +1,5 @@
+#include "main.h"
 
-#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <eigen3/Eigen/Eigen>
-#include <random>
-#include <ctime>
-#include <string>
-
-#include "simulator/robot.h"
-
-static std::random_device rd;
-static std::mt19937_64 gen(rd());
-static std::normal_distribution gaussian_distr;
-
-int main(){
-
-    for (int i = 0; i < 5; i++) {
-        double n = gaussian_distr(gen);
-        std::cout << "random number: " << n << std::endl;
-    }
-
+int main() {
     return 0;
 }
