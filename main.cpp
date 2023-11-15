40fe104339cc5942f6abb25b6ecdd9c118eafa0b
TomBim <washingtonbim@hotmail.com>
2023-11-15 15:10:42 -0300

    +some cmds

--- main.cpp
+++ main.cpp
@@ -10,16 +10,26 @@ int main() {
 
     Simulator sim;
     CmdSignal cmds;
-    CmdSignalBase *cmdPtr;
+    CmdSignalBase *cmdPtr, *cmdPtrMinus;
 
     // choose my cmd
-    ConstantCmdSignal cmd(1);
-    cmdPtr = (CmdSignalBase*) &cmd;    
-    cmds.addCmd_robotStateV(cmdPtr, 0, 10);
+    ConstantCmdSignal cmd(1), cmdMinus(-1);
+    cmdPtr = (CmdSignalBase*) &cmd;
+    cmdPtrMinus = (CmdSignalBase*) &cmdMinus;
+    cmds.addCmd_robotStateV(cmdPtr, 0, 1);
+    cmds.addCmd_robotStateVn(cmdPtr, 1, 2);
+    cmds.addCmd_robotStateOmega(cmdPtr, 2, 3);
+    cmds.addCmd_robotStateV(cmdPtrMinus, 3, 4);
+    cmds.addCmd_robotStateVn(cmdPtrMinus, 4, 5);
+    cmds.addCmd_robotStateOmega(cmdPtrMinus, 5, 6);
+    cmds.addCmd_robotStateV(cmdPtr, 6, 10);
+    cmds.addCmd_robotStateVn(cmdPtr, 7, 10);
+    cmds.addCmd_robotStateOmega(cmdPtr, 8, 9);
+
 
     // simulate
     std::cout << "starting simulation" << std::endl << std::endl;
-    sim.simulateFor(1, cmds);
+    sim.simulateFor(10, cmds);
     std::cout << "finished simulation! Finishing program" << std::endl;
 
     return 0;
